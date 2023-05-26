0c2724bc6e8673fbe9453edc80762d8f38eb9c09
nicolasdosantos <nicolaspichiteli245@gmail.com>
2023-05-26 10:41:12 -0300

    -----------

--- pythonTI/condicoes2/exerc1.py
+++ pythonTI/condicoes2/exerc1.py
@@ -1,9 +1,8 @@
 time1 = int(input("Coloquei seu placar do primeiro time"))
 time2 = int(input("Coloquei seu placar do segundo time"))
-
 if time1 == time2:
-    print("A partida terminou em empate")
+    print(f"A partida terminou em empate com o resultado de {time1}x{time2}")
 elif time1 > time2:
-    print("A vitoria é do time 1")
+    print(f"A vitoria é do time 1 com o placar de {time1}x{time2}")
 elif time1 < time2:
-    print("A vitoria é do time 2")
+    print(f"A vitoria é do time 2 com o placar de {time1}x{time2}")
--- pythonTI/condicoes2/exerc2.py
+++ pythonTI/condicoes2/exerc2.py
@@ -1,8 +1,6 @@
 c = int(input("Coloquei o valor da compra"))
 p = int(input("Coloque em quantas prestações será pago"))
-
 calculo = c/p
-
 if calculo > 500:
     print(f"Você não conseguira pagar, pois cada prestação ficou {calculo}")
 elif calculo < 500:
