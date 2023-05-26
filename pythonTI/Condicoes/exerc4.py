8874598ccd8068b940c3ef86adea2ea31a0585fb
nicolasdosantos <nicolaspichiteli245@gmail.com>
2023-05-26 08:39:17 -0300

    ---------------

---
+++ pythonTI/Condicoes/exerc4.py
@@ -0,0 +1,7 @@
+data = int(input("Coloquei seu ano de nascimento"))
+conta = 2023-data
+if conta < 18:
+    print("Voce é menor de idade")
+else:
+    print("Voce ja é maior de idade")
+
---
+++ pythonTI/Condicoes/exerc5.py
@@ -0,0 +1,8 @@
+num1 = int(input("Coloquei seu primeiro numero"))
+num2 = int(input("Coloquei seu segundo numero"))
+
+if num1 == num2:
+    print("Seus numeros são iguais")
+else:
+    print("Seus numeros são diferente")
+
