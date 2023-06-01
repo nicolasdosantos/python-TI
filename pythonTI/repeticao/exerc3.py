b1e3e49070becb2f456c7d7350987dc83841c08b
nicolasdosantos <nicolaspichiteli245@gmail.com>
2023-06-01 09:00:18 -0300

    -----------

--- pythonTI/repeticao/exerc3.py
+++ pythonTI/repeticao/exerc3.py
@@ -1,12 +1,10 @@
-from numpy.core.defchararray import lower, upper
-
 mediaF = 0
 mediaM = 0
 mediaG = 0
 idadeF = 0
 idadeM = 0
 idadeG = 0
-for i in range(4):
+for i in range(10):
     g = int(input("Qual é seu genero?\nFeminino(1)\nMasculino(2)"))
     if g == 1:
         idade = int(input("Qual é a sua idade?"))
@@ -18,7 +16,7 @@ for i in range(4):
         idade = int(input("Qual é a sua idade?"))
         mediaG += 1
         mediaM += 1
-        idadeM += idadeM
+        idadeM += idade
         idadeG += idade
 calculoF = idadeF/mediaF
 calculoM = idadeM/mediaM
