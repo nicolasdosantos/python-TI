f27aec4031447b6e3fa4a10e7e455808e25dfb11
nicolasdosantos <nicolaspichiteli245@gmail.com>
2023-06-02 09:11:39 -0300

    ---------------------

--- pythonTI/repeticao/while/exerc1.py
+++ pythonTI/repeticao/while/exerc1.py
@@ -1,29 +1,3 @@
-#while 30 < 20:
-#    print("Ola mundo")
-
-#while 10 < 20:
-#    print("Ola mundo")
-
-#num = 0
-#while num < 20:
-#    print(num)
-#    num += 1
-
-#Res = 'S'
-#while Res == "S":
-#    num = int(input("Digite um numero\nR:"))
-#    if num > 999:
-#        print("Seu numero é muito grande")
-#       break
-#    Res = str(input('Deseja continuar?S/N\nR:'))
-
-#|while True:
-#    ini = str(input('Digite comecar'))
-#    if ini != "comecar":
-#        print("Não digitou certo reveja")
-#    else:
-#        break
-
 nota = 0
 media = 0
 while True:
