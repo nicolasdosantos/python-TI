18e8b6e3bc013a7f89c3bdef62965558b09b3e19
nicolasdosantos <nicolaspichiteli245@gmail.com>
2023-06-02 10:36:48 -0300

    -------------------

--- pythonTI/repeticao/lista For/exerc4.py
+++ pythonTI/repeticao/lista For/exerc4.py
@@ -1,5 +1,3 @@
-#depois pergunte e leia a quantidade de clientes que chegaram até ocupar toda a capacidade do restaurante e quando lotar imprima na tela «Restaurante lotado, não há mais mesas disponíveis».
-
 max = 50
 cliente = 0
 for i in range(max):
