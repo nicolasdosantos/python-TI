d411ebe1866dfebf36b35d102fdf0694d852a91c
nicolasdosantos <nicolaspichiteli245@gmail.com>
2023-06-16 09:03:06 -0300

    -------------

--- pythonTI/Composta/Tupla/exerc5.py
+++ pythonTI/Composta/Tupla/exerc5.py
@@ -1,9 +1,9 @@
 itens = ("Fruta - 0", "Jogo - 1","Caneta - 2", "Bolsa - 3")
-f = ("Goiaba, Quantidade: 15, Preço: 10,99Kg")
-j = ("Nome: Miguel, Telefone :18 99614-8856, Email:miguel@gmail.com")
-c = ("Nome: Basinga, Telefone :18 99614-8856, Email:basinga@gmail.com")
-b
-p = int(input("Qual contato voce quer"))
+f = ("Fruta: Goiaba, Quantidade: 15, Preço: 10,99Kg")
+j = ("Jogo: Overcooked, Quantidade: 10, Preço: 60,00")
+c = ("Marca: Bic, Quantidade: 6, Preço: 02,00")
+b = ("Marca: kipling, Quantidade: 1, Preço: 800,00")
+p = int(input("Qual Item voce quer as informações"))
 
 if p == 0:
     print(f)
