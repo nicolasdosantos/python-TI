e61721ab154726050bd8a777c39b64d585418de3
nicolasdosantos <nicolaspichiteli245@gmail.com>
2023-06-16 08:58:52 -0300

    ----------

---
+++ pythonTI/Composta/Tupla/exerc4.py
@@ -0,0 +1,12 @@
+contato = ("Nicolas - 0", "Miguel - 1","Basqueroto - 2")
+nicolas = ("Nome: Nicolas, Telefone :18 99614-8856, Email:nicolas@gmail.com")
+Miguel = ("Nome: Miguel, Telefone :18 99614-8856, Email:miguel@gmail.com")
+Basqueroto = ("Nome: Basinga, Telefone :18 99614-8856, Email:basinga@gmail.com")
+p = int(input("Qual contato voce quer"))
+
+if p == 0:
+    print(nicolas)
+elif p == 1:
+    print(Miguel)
+elif p == 2:
+    print(Basqueroto)
---
+++ pythonTI/Composta/Tupla/exerc5.py
@@ -0,0 +1,15 @@
+itens = ("Fruta - 0", "Jogo - 1","Caneta - 2", "Bolsa - 3")
+f = ("Goiaba, Quantidade: 15, Preço: 10,99Kg")
+j = ("Nome: Miguel, Telefone :18 99614-8856, Email:miguel@gmail.com")
+c = ("Nome: Basinga, Telefone :18 99614-8856, Email:basinga@gmail.com")
+b
+p = int(input("Qual contato voce quer"))
+
+if p == 0:
+    print(f)
+elif p == 1:
+    print(j)
+elif p == 2:
+    print(c)
+elif p == 3:
+    print(b)
