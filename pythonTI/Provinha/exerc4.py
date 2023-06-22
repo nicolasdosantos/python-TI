e36c1ea83fdd7e26afb75b5acc0ae13b32b52b08
nicolasdosantos <nicolaspichiteli245@gmail.com>
2023-06-22 15:08:32 -0300

    Exerc4

--- pythonTI/Provinha/exerc4.py
+++ pythonTI/Provinha/exerc4.py
@@ -1,9 +1,17 @@
+#NOME: Nicolas - QUESTÃO: Figurinha
+
+# INPUT PARA SABER A QUANTIDADE DE REPETIÇÃO
 teste = int(input("Quantas vezes voce ira testar o programa?"))
 
+# FOR PARA FZR A REPETIÇÃO
 for i in range(teste):
+    # CRIAÇÃO DAS VARIAVEIS
     num1 = int(input("Digite seu primeiro numero inteiro"))
     num2 = int(input("Digite seu segundo numero inteiro"))
+
+    #SE O NUMERO POR PAR, ELE IRA PEGAR O RESTO DA DIVISÃO DO NUMERO 2 PELO 1, E O RESTO DA DIVISÃO SERA O DIVISOR DOS DOIS
     if num1 %2 == 0:
         print(f"O MDC dos numero {num1} e {num2}, é {num2 % num1}")
+    #SE O NUMERO POR IMPAR, ELE IRA PEGAR O RESTO DA DIVISÃO DO NUMERO 1 PELO 2, E O RESTO DA DIVISÃO SERA O DIVISOR DOS DOIS
     else:
         print(f"O MDC dos numero {num1} e {num2}, é {num1 % num2}")
