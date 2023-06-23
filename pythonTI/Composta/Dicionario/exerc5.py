31a09f796d6515e2e9833b78b44b14b08b5a0168
nicolasdosantos <nicolaspichiteli245@gmail.com>
2023-06-23 16:28:17 -0300

    Certo

--- pythonTI/Composta/Dicionario/exerc5.py
+++ pythonTI/Composta/Dicionario/exerc5.py
@@ -1,7 +1,7 @@
 notas = {}
 
 while True:
-    n = int(input("Digite um instrumento avaliativo\n"))
+    n = str(input("Digite um instrumento avaliativo\n"))
     notas[n] = float(input("Digite sua nota da avaliação\n"))
     p = str(input("Voce deseja continuar?S ou N")).lower()
 
