c9bf2b714a1b62df1cc2ebb9134df84bfbfc92d2
nicolasdosantos <nicolaspichiteli245@gmail.com>
2023-06-23 10:36:31 -0300

    Exercicios

--- pythonTI/Composta/Lista/exerc7.py
+++ pythonTI/Composta/Lista/exerc7.py
@@ -3,6 +3,9 @@ num = []
 p = int(input("Quantas vezes voce ira testas o programa?\n"))
 
 for i in range(p):
-    num.append(int(input("Digite algum numero\n")))
-    if num == num:
-        print("Esse numero ja foi digitado")
+    x = int(input("Digite algum numero\n"))
+    if x == 0:
+        break
+    elif x not in num:
+        num.sort()
+        print(num)
