2d04bbe38c1d0bc6abbfc5e609d51b212a8f1c3c
nicolasdosantos <104533715+nicolasdosantos@users.noreply.github.com>
2023-08-31 11:11:51 -0300

    ----------

--- OBJ/Encapsulamento/Exerc2.py
+++ OBJ/Encapsulamento/Exerc2.py
@@ -3,8 +3,14 @@ class Conta:
         self.__saldo = saldo
         self.titular = titular
 
+
     def depositar(self):
         q = int(input('Qual a quantidade sera depositada'))
         q += self.__saldo
         print("Deposito feito com sucesso")
         print("Seu saldo atual é de", self.__saldo)
+
+
+    def sacar(self):
+        a = int(input("Quanto vc ira sacar?"))
+        print("Sacando:", a, "reais")
